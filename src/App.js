51f251d4e88d6ced7d735a5244979cee87d9ec29
hackyu <hackyu.kr@gmail.com>
2021-06-11 15:47:20 +0900

    Boxing Skill

--- src/App.js
+++ src/App.js
@@ -1,17 +1,62 @@
 import React from 'react';
-//import Post from './Post';
 
-function Post(props){
+const sportsList = [
+  {
+    name:"Jab",
+    image:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.Ut0a24jpof3SOt68i_ZHRAHaEK%26pid%3DApi&f=1"
+  },
+  
+  {
+    name:"Upper",
+    image:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.scduGgDh2b1q8TbGCmcDsAHaFH%26pid%3DApi&f=1"
+  },
+  
+  {
+    name:"Hook",
+    image:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.CuRHyX32zcra9L0F0pjdkAHaEJ%26pid%3DApi&f=1"
+  },
+  
+  {
+    name:"Sway",
+    image:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.g3Iw3xHMyOQKKVkun2m-zwAAAA%26pid%3DApi&f=1"
+  },
+  
+  {
+    name:"Ducking",
+    image:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.HAiDtkh5fbwK0PW5ZlhN2wHaFP%26pid%3DApi&f=1"
+  },
+  
+  {
+    name:"Weaving",
+    image:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.PHDYsRKRBnBxSFHRCQ2t9wHaFj%26pid%3DApi&f=1"
+  },
+
+  {
+    name:"Slipping",
+    image:"https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.sneakpunch.com%2Fwp-content%2Fuploads%2F2013%2F04%2Fthe_slip2_col.png&f=1&nofb=1"
+  }
+  
+];
+
+// function GetBoxingSkill({name, image}){
+//   //console.log(name);
+//   console.log(name, image);
+//   return (
+//     <div>
+//       The Skill Name: {name} <br/>
+//       <img src={image}/> <br/>
+//     </div>
+//   );
+// }
+
+function GetBoxingSkill(props){
+  //console.log(name);
   console.log(props);
   return (
-    <div>i hate {props.a}</div>
-  );
-}
-
-function Get({a}){
-  console.log(a);
-  return (
-    <div>i hate {a}</div>
+    <div>
+      The Skill Name: {props.name} <br/>
+      <img src={props.image}/> <br/>
+    </div>
   );
 }
 
@@ -19,12 +64,11 @@ function App() {
   return (
     // html type statement
     <div>
-      <Post a="1234"/>
-      <Post b="5678"/>
-      
-      <Get a="1234"/>
-      <Get b="5678"/>
+      {sportsList.map(skill => (
+        <GetBoxingSkill name={skill.name} image={skill.image}/>
+      ))}
     </div>
   );
 }
+
 export default App;
